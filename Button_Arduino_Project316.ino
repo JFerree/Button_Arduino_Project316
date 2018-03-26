0116bee06967957ffb55cf0536f60e35868c2605
Jim Ferree <j_ferree@yahoo.com>
2018-03-25 20:31:57 -0500

    Formatting Changes

--- Button_Arduino_Project316.ino
+++ Button_Arduino_Project316.ino
@@ -6,41 +6,29 @@
 #define DATA_PIN 3
 
 //led array set up
-
 CRGB leds[NUM_LEDS];
 
-int numLedsToLight;
-
 #define BRIGHTNESS          96
-
 #define FRAMES_PER_SECOND  120
-
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
 //button setup
-
 const int buttonPin = 2;  
-
 int buttonState = 0;   
 
 
 void setup() {
-  
 // sanity check delay - allows reprogramming if accidently blowing power w/leds
     delay(2000);
-
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-    numLedsToLight = 0;
-
-    pinMode(buttonPin, INPUT);
-    
+    pinMode(buttonPin, INPUT);    
 }
+
 typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { turnOnLeds, turnOffLeds };
 
 //sets the pattern equal to the current button press count
 uint8_t gCurrentPatternNumber = count; // Index number of which pattern is current
-
 
 void loop() {
  gPatterns[gCurrentPatternNumber]();
@@ -51,11 +39,13 @@ void loop() {
   FastLED.delay(1000/FRAMES_PER_SECOND); 
   counter();
 }
+
 void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
+
 void turnOnLeds()
 {
   fill_solid( leds,NUM_LEDS, CRGB(205,205,205));
@@ -64,6 +54,7 @@ void turnOnLeds()
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND); 
 }
+
 void turnOffLeds()
 {FastLED.clear();
   // send the 'leds' array out to the actual LED strip
@@ -71,6 +62,7 @@ void turnOffLeds()
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND); 
 }
+
 void counter()
 {if (digitalRead(buttonPin) == HIGH) {
    nextPattern(); 
